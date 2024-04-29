16df6e6c393d731f36357379066156d51642d2d1
Philipp Keese <me@philippkeese.me>
2024-04-30 00:51:09 +0200

    feat: add stricter NaN checks and display method

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 /// A NaN-tagged value supporting f64, i32, u32, booleans, null and arbitrary pointers.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Value {
@@ -5,12 +7,25 @@ pub struct Value {
 }
 
 #[repr(u64)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Tag {
-    Null = 0 << 48,
-    False = 1 << 48,
-    True = 2 << 48,
-    I32 = 3 << 48,
-    U32 = 4 << 48,
+    Nan = 0,
+    Null,
+    False,
+    True,
+    I32,
+    U32,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Type {
+    Double,
+    Null,
+    False,
+    True,
+    I32,
+    U32,
+    Pointer,
 }
 
 
@@ -18,9 +33,9 @@ impl Value {
     /// Sign bit, used to distinguish between pointers and other values.
     const SIGN_BIT: u64 = 1 << 63;
     /// Special NaN value for representing NaNs and non-f64 values.
-    const NAN: u64 = 0x7ff8_0000_0000_0000;
+    const QUIET_NAN: u64 = 0x7ff8_0000_0000_0000;
     /// Bits set for pointers.
-    const POINTER_BITS: u64 = Self::SIGN_BIT | Self::NAN;
+    const POINTER_BITS: u64 = Self::SIGN_BIT | Self::QUIET_NAN;
 
     /// Mask used for tag bits. Just 3 bits, shifted to the left by 48 to stay clear of pointer bits.
     const TAG_MASK: u64 = 0b111 << 48;
@@ -29,36 +44,56 @@ impl Value {
     const FALSE: Self = Self::new_primitive(Tag::False);
     const TRUE: Self = Self::new_primitive(Tag::True);
     const NULL: Self = Self::new_primitive(Tag::Null);
+    const NAN: Self = Self::new_primitive(Tag::Nan);
 
     /// Create a primitive tagged value.
     const fn new_primitive(tag: Tag) -> Self {
-        Self { bits: tag as u64 | Self::NAN }
+        let tag = (tag as u64) << 48;
+        Self { bits: tag | Self::QUIET_NAN }
     }
 
     /// Create a tagged value with a value.
     const fn new(tag: Tag, value: u64) -> Self {
-        Self { bits: tag as u64 | Self::NAN | value }
+        let tag = (tag as u64) << 48;
+        Self { bits: tag | Self::QUIET_NAN | value }
     }
 
-    /// Get tag bits.
-    fn tag(self) -> u64 {
-        self.bits & Self::TAG_MASK
+    /// Get type of value.
+    fn get_type(self) -> Type {
+        if self.is_double() {
+            return Type::Double;
+        } else if self.is_pointer() {
+            return Type::Pointer;
+        }
+        let tag = (self.bits & Self::TAG_MASK) >> 48;
+        match tag {
+            x if x == Tag::Null as u64 => Type::Null,
+            x if x == Tag::False as u64 => Type::False,
+            x if x == Tag::True as u64 => Type::True,
+            x if x == Tag::I32 as u64 => Type::I32,
+            x if x == Tag::U32 as u64 => Type::U32,
+            _ => unreachable!()
+        }
     }
 
     /// Is this value a f64?
     /// Valid f64 are not NaN, or if they are, they have a specific bit pattern.
     pub fn is_double(self) -> bool {
-        (self.bits & Self::NAN) != Self::NAN
+        if self == Self::NAN {
+            true
+        } else {
+            (self.bits & Self::QUIET_NAN) != Self::QUIET_NAN
+        }
     }
 
     /// Is this value an i32?
     pub fn is_i32(self) -> bool {
-        self.tag() == Tag::I32 as u64
+        self.get_type() == Type::I32
     }
 
     /// Is this value an u32?
     pub fn is_u32(self) -> bool {
-        self.tag() == Tag::U32 as u64
+        self.get_type() == Type::U32
     }
 
     /// Is this value a boolean?
@@ -134,8 +169,7 @@ impl Value {
     /// # Safety
     /// The caller must be certain that the value is an i32.
     pub unsafe fn as_i32_unchecked(self) -> i32 {
-        let bits = self.bits as u32 as i32;
-        bits
+        self.bits as u32 as i32
     }
 
     /// Unchecked conversion to u32.
@@ -163,7 +197,7 @@ impl Value {
 impl From<f64> for Value {
     fn from(value: f64) -> Self {
         if value.is_nan() {
-            Self { bits: Self::NAN }
+            Self::NAN
         } else {
             Self { bits: value.to_bits() }
         }
@@ -205,6 +239,22 @@ impl<T> From<*const T> for Value {
     }
 }
 
+impl Display for Value {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        unsafe {
+            match self.get_type() {
+                Type::Double => write!(f, "{}", self.as_double_unchecked()),
+                Type::False => write!(f, "false"),
+                Type::True => write!(f, "true"),
+                Type::Null => write!(f, "null"),
+                Type::I32 => write!(f, "{}", self.as_i32_unchecked()),
+                Type::U32 => write!(f, "{}", self.as_u32_unchecked()),
+                _ => write!(f, "{:p}", self.as_pointer_unchecked::<u8>()),
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -227,7 +277,19 @@ mod tests {
         assert!(!value.is_bool());
         assert!(!value.is_null());
         assert_eq!(value.as_double(), Some(42.0));
-        unsafe { assert_eq!(value.as_double_unchecked(), 42.0); }
+        assert_eq!(value.as_double_unchecked(), 42.0);
+    }
+
+    #[test]
+    fn test_nan() {
+        let value = Value::from(f64::NAN);
+        assert!(value.is_double());
+        assert!(!value.is_i32());
+        assert!(!value.is_u32());
+        assert!(!value.is_bool());
+        assert!(!value.is_null());
+        assert!(value.as_double().unwrap().is_nan());
+        assert!(value.as_double_unchecked().is_nan());
     }
 
     #[test]
@@ -263,7 +325,7 @@ mod tests {
         assert!(value.is_bool());
         assert!(!value.is_null());
         assert_eq!(value.as_bool(), Some(true));
-        unsafe { assert_eq!(value.as_bool_unchecked(), true); }
+        unsafe { assert!(value.as_bool_unchecked()); }
     }
 
     #[test]
@@ -283,5 +345,22 @@ mod tests {
         assert!(value.is_pointer());
         assert_eq!(value.as_pointer(), Some(ptr));
         unsafe { assert_eq!(value.as_pointer_unchecked(), ptr); }
+    }
+
+    #[test]
+    fn test_display() {
+        let value = Value::from(42.0);
+        assert_eq!(format!("{}", value), "42");
+        let value = Value::from(-1234);
+        assert_eq!(format!("{}", value), "-1234");
+        let value = Value::from(1234u32);
+        assert_eq!(format!("{}", value), "1234");
+        let value = Value::from(true);
+        assert_eq!(format!("{}", value), "true");
+        let value = Value::from(());
+        assert_eq!(format!("{}", value), "null");
+        let ptr = 0xdead_beef as *const u8;
+        let value = Value::from(ptr);
+        assert_eq!(format!("{}", value), format!("{:p}", ptr));
     }
 }
